e114af4c88f3e3a7e92943f7ede607f39d146440
Osman Bin Nasir <osmanbinnasir19@gmail.com>
2026-07-13 02:04:46 +0530

    Refactor Hamiltonian path algorithm for clarity

--- Hamiltonian.java
+++ Hamiltonian.java
@@ -9,42 +9,58 @@ public class Hamiltonian {
     };
 
     static int n = 5;
-
-    // Stores the Hamiltonian path
     static int path[] = new int[n];
 
     public static void main(String[] args) {
-    	path[0]=0;
-	hamiltonian(1);
+
+        for (int i = 0; i < n; i++)
+            path[i] = -1;
+
+        path[0] = 0;
+
+        hamiltonian(1);
     }
 
-    static boolean isSafe(int v, int pos){
-	    if(graph[pos-1][v] == 0)
-		    return false;
+    static boolean isSafe(int v, int pos) {
 
-	    for (int i = 0; i < n; i++) {
-	    	if(path[i] == v)
-			return false;
-	    }
-	    return true;
+        // Check if connected to previous vertex
+        if (graph[path[pos - 1]][v] == 0)
+            return false;
+
+        // Check if already included
+        for (int i = 0; i < pos; i++) {
+            if (path[i] == v)
+                return false;
+        }
+
+        return true;
     }
 
-    static void hamiltonian(int pos){
-	if(pos==n){
-		if(graph[path[n-1]][path[0]] == 1){ 
-			for (int i = 0; i < n; i++) {
-				System.out.print(path[i]+" ");
-			}
-			System.out.print(path[0]);
-		}
-		return;
-	}
-	for (int v = 0; v < n; v++) {
-		if(isSafe(v,pos)){
-			path[pos]=v;
-			hamiltonian(pos+1);
-			path[pos]=-1;
-		}
-	}
+    static void hamiltonian(int pos) {
+
+        if (pos == n) {
+
+            if (graph[path[n - 1]][path[0]] == 1) {
+
+                for (int i = 0; i < n; i++)
+                    System.out.print(path[i] + " ");
+
+                System.out.println(path[0]);
+            }
+
+            return;
+        }
+
+        for (int v = 1; v < n; v++) {
+
+            if (isSafe(v, pos)) {
+
+                path[pos] = v;
+
+                hamiltonian(pos + 1);
+
+                path[pos] = -1;
+            }
+        }
     }
 }
